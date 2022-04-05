2a3acec5d57979568e8a26fc684dab138118e248
“saralemus7” <saralemus70@gmail.com>
2022-04-04 20:02:11 -0400

    added data and initial R files

---
+++ R/worksjanson.R
@@ -0,0 +1,31 @@
+#' Northern Ireland baby names
+#'
+#' Full baby name data from 1997 to 2020 for Northern Ireland
+#' from the Northern Ireland Statistics and Research Agency.
+#'
+#' @name worksjanson
+#' @docType data
+#' @format A data frame with 22,596 observations on 6 variables.
+#' \describe{
+#'   \item{year}{A year (1997-2020).}
+#'   \item{sex}{Sex, `M` for male and `F` for female.}
+#'   \item{name}{A name.}
+#'   \item{n}{Count of name within year and sex.}
+#'   \item{rank}{Rank of name within year and sex.}
+#'   \item{nation}{Nation of data source: `Northern Ireland`.}
+#'   }
+#' @source
+#' Northern Ireland Statistics and Research Agency. 2021. [Baby Names Statistics Boys and Girls](https://www.nisra.gov.uk/publications/baby-names-2020-0).
+#' @examples
+#' library(ggplot2)
+#' library(dplyr)
+#'
+#' worksjanson %>%
+#'   filter(name == "Joseph") %>%
+#'   ggplot(aes(x = year, y = n)) +
+#'   geom_line() +
+#'   labs(
+#'     title = "Popularity of the name `Joseph` in Northern Ireland",
+#'     x = "Year", y = "Number of babies"
+#'     )
+"worksjanson"
