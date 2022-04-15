8abe4353bb9f01c49a3c94f310f81b0a89bc2589
“saralemus7” <saralemus70@gmail.com>
2022-04-14 22:48:43 -0400

    wrote a couple basic tests, updated readme with doi link

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,3 +1,9 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("gardner nrow matches expected", {
+  expect_equal(2325, nrow(worksgardner))
+})
+test_that("janson nrow matches expected", {
+  expect_equal(1634, nrow(worksjanson))
+})
+test_that("artists nrow matches expected", {
+  expect_equal(3162, nrow(artists))
 })
